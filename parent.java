b6102a11691862b3e851b495b109c32d8354cad5
tharun reddy <tharunreddy057@gmail.com>
2021-09-11 11:43:46 +0530

    tfguyahasjx

---
+++ parent.java
@@ -0,0 +1,13 @@
+public class parent{
+public static void  main(String args[]){
+System.out.println("craeted the c ustomer");
+System.out.println("craeted the f ustomer");
+System.out.println("craeted the e ustomer");
+System.out.println("craeted the f ustomer");
+System.out.println("craeted the f ustomer");
+
+
+
+
+}
+}
